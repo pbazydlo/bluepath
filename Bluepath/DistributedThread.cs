a9c1b525165b5e2ac04f8b03f19693fde8d641c5
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2014-03-06 22:53:54 +0100

    Adding missing files

---
+++ Bluepath/DistributedThread.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Bluepath
+{
+    public class DistributedThread
+    {
+
+        public static DistributedThread Create(Func<object[], object> function)
+        {
+            return new DistributedThread()
+            {
+                function = function
+            };
+        }
+
+        public void Start(object[] parameters)
+        {
+            this.executor = new Thread(() =>
+            {
+                this.Result = this.function(parameters);
+            });
+
+            this.executor.Start();
+        }
+
+        public void Join()
+        {
+            this.executor.Join();
+        }
+
+        public object Result { get; private set; }
+
+        private Thread executor;
+        
+        private Func<object[], object> function;
+
+        private DistributedThread() { }
+    }
+}

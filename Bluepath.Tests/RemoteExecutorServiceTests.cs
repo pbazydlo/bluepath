cfd5a023a34ddb64e7c095af1d5fe517662cd5e2
Zachariusz Karwacki <zachariusz@karwacki.at>
2014-03-07 16:39:46 +0100

    Test: exception is user code should get caught by our framework

--- Bluepath.Tests/RemoteExecutorServiceTests.cs
+++ Bluepath.Tests/RemoteExecutorServiceTests.cs
@@ -21,6 +21,8 @@
             var service = new RemoteExecutorService();
             var serializedMethodHandle = global::Bluepath.Executor.MethodHandleSerializer.GetSerializedMethodHandle(testMethod);
             var eid = service.Initialize(serializedMethodHandle);
+
+            // the following method was private, but it should be public to allow this kind of test
             var executor = RemoteExecutorService.GetExecutor(eid);
 
             service.Execute(eid, new object[] { 1, 2 });
@@ -34,6 +36,29 @@
             result.ExecutorState.ShouldBe(RemoteExecutorServiceResult.State.Finished);
             result.Result.ShouldBe(3);
             result.Error.ShouldBe(null);
+            result.ElapsedTime.HasValue.ShouldBe(true);
+        }
+
+        [TestMethod]
+        public void RemoteExecutorServiceExceptionInUserCodeShouldGetCaughtTest()
+        {
+            var testMethod = new Func<object>(() => { throw new Exception("test"); });
+
+            var service = new RemoteExecutorService();
+            var serializedMethodHandle = MethodHandleSerializer.GetSerializedMethodHandle(testMethod);
+            var eid = service.Initialize(serializedMethodHandle);
+            var executor = RemoteExecutorService.GetExecutor(eid);
+
+            service.Execute(eid, null);
+
+            // wait for the worker thread to complete
+            executor.Join();
+
+            // use method exposed by the service to get computation state
+            var result = service.TryJoin(eid);
+
+            result.ExecutorState.ShouldBe(RemoteExecutorServiceResult.State.Faulted);
+            result.Error.InnerException.Message.ShouldBe("test");
             result.ElapsedTime.HasValue.ShouldBe(true);
         }
     }

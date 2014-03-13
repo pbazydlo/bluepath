3fa6d6c1b6ae2aaaf5293277eee16ae84c3b4cd3
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2014-03-13 11:00:37 +0100

    Preparing for running remote executors in distributed thread.

--- Bluepath/Threading/DistributedThread.cs
+++ Bluepath/Threading/DistributedThread.cs
@@ -1,33 +1,56 @@
 ﻿namespace Bluepath.Threading
 {
     using System;
-
+    using System.Linq;
     using global::Bluepath.Executor;
+    using System.Collections.Generic;
 
     /// <summary>
     /// TODO: Description, Remote Execution, Choosing executing node
     /// </summary>
     public class DistributedThread
     {
-        private ILocalExecutor executor;
+        private IExecutor executor;
 
         private Func<object[], object> function;
 
         private DistributedThread() { }
 
-        public static DistributedThread Create(Func<object[], object> function)
+        public static readonly List<ServiceReferences.IRemoteExecutorService> RemoteServices = new List<ServiceReferences.IRemoteExecutorService>();
+
+        public static DistributedThread Create(Func<object[], object> function, ExecutorSelectionMode mode = ExecutorSelectionMode.LocalOnly)
         {
             return new DistributedThread()
             {
-                function = function
+                function = function,
+                Mode = mode
             };
         }
 
+        public ExecutorSelectionMode Mode { get; private set; }
+
         public void Start(object[] parameters)
         {
-            // TODO: replace with RemoteExecutor
-            this.executor = new LocalExecutor();
-            this.executor.Initialize(this.function);
+            switch (this.Mode)
+            {
+                case ExecutorSelectionMode.LocalOnly:
+                    var localExecutor = new LocalExecutor();
+                    localExecutor.Initialize(this.function);
+                    this.executor = localExecutor;
+                    break;
+                case ExecutorSelectionMode.RemoteOnly:
+                    var remoteExecutor = new RemoteExecutor();
+                    var service = RemoteServices.FirstOrDefault();
+                    if(service == null)
+                    {
+                        throw new NullReferenceException("No remote service was specified in DistributedThread.RemoteServices!");
+                    }
+
+                    remoteExecutor.Initialize(service, this.function);
+                    this.executor = remoteExecutor;
+                    break;
+            }
+
             this.executor.Execute(parameters);
         }
 
@@ -42,6 +65,12 @@
             {
                 return this.executor.Result;
             }
+        }
+
+        public enum ExecutorSelectionMode : int
+        {
+            LocalOnly = 0,
+            RemoteOnly = 1
         }
     }
 }

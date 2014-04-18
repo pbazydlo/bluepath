c186f96dd7fd9d88021f8437ad7db18919199d87
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2014-04-18 12:45:34 +0200

    Preparing to implement selectmany

--- Bluepath/DLINQ/DistributedEnumerable.cs
+++ Bluepath/DLINQ/DistributedEnumerable.cs
@@ -64,5 +64,121 @@ namespace Bluepath.DLINQ
 
             return resultCollection.Count;
         }
+
+        /// <summary>
+        /// Projects in parallel each element of a sequence to an IEnumerable{T} 
+        /// and flattens the resulting sequences into one sequence.
+        /// </summary>
+        /// <typeparam name="TSource">The type of elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of the sequence returned by <B>selector</B>.</typeparam>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns>A sequence whose elements are the result of invoking the one-to-many transform 
+        /// function on each element of the input sequence.</returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="selector"/> is a null reference (Nothing in Visual Basic).
+        /// </exception>
+        public static DistributedQuery<TResult> SelectMany<TSource, TResult>(
+            this DistributedQuery<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            throw new NotImplementedException();
+            //return new SelectManyQueryOperator<TSource, TResult, TResult>(source, selector, null, null);
+        }
+
+        /// <summary>
+        /// Projects in parallel each element of a sequence to an IEnumerable{T}, and flattens the resulting 
+        /// sequences into one sequence. The index of each source element is used in the projected form of 
+        /// that element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TResult">The type of the elements of the sequence returned by <B>selector</B>.</typeparam>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="selector">A transform function to apply to each element.</param>
+        /// <returns>A sequence whose elements are the result of invoking the one-to-many transform 
+        /// function on each element of the input sequence.</returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="selector"/> is a null reference (Nothing in Visual Basic).
+        /// </exception>
+        public static DistributedQuery<TResult> SelectMany<TSource, TResult>(
+             this DistributedQuery<TSource> source, Func<TSource, int, IEnumerable<TResult>> selector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (selector == null) throw new ArgumentNullException("selector");
+
+            throw new NotImplementedException();
+            //return new SelectManyQueryOperator<TSource, TResult, TResult>(source, null, selector, null);
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence to an IEnumerable{T}, 
+        /// flattens the resulting sequences into one sequence, and invokes a result selector 
+        /// function on each element therein.
+        /// </summary>
+        /// <typeparam name="TSource">The type of elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TCollection">The type of the intermediate elements collected by <paramref name="collectionSelector"/>.</typeparam>
+        /// <typeparam name="TResult"></typeparam>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="collectionSelector">A transform function to apply to each source element; 
+        /// the second parameter of the function represents the index of the source element.</param>
+        /// <param name="resultSelector">A function to create a result element from an element from 
+        /// the first sequence and a collection of matching elements from the second sequence.</param>
+        /// <returns>A sequence whose elements are the result of invoking the one-to-many transform 
+        /// function <paramref name="collectionSelector"/> on each element of <paramref name="source"/> and then mapping 
+        /// each of those sequence elements and their corresponding source element to a result element.</returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="collectionSelector"/> or
+        /// <paramref name="resultSelector"/> is a null reference (Nothing in Visual Basic).
+        /// </exception>
+        public static DistributedQuery<TResult> SelectMany<TSource, TCollection, TResult>(
+            this DistributedQuery<TSource> source, Func<TSource, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (collectionSelector == null) throw new ArgumentNullException("collectionSelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            throw new NotImplementedException();
+            //return new SelectManyQueryOperator<TSource, TCollection, TResult>(source, collectionSelector, null, resultSelector);
+        }
+
+        /// <summary>
+        /// Projects each element of a sequence to an IEnumerable{T}, flattens the resulting 
+        /// sequences into one sequence, and invokes a result selector function on each element 
+        /// therein. The index of each source element is used in the intermediate projected 
+        /// form of that element.
+        /// </summary>
+        /// <typeparam name="TSource">The type of elements of <paramref name="source"/>.</typeparam>
+        /// <typeparam name="TCollection">The type of the intermediate elements collected by 
+        /// <paramref name="collectionSelector"/>.</typeparam>
+        /// <typeparam name="TResult">The type of elements to return.</typeparam>
+        /// <param name="source">A sequence of values to project.</param>
+        /// <param name="collectionSelector">A transform function to apply to each source element; 
+        /// the second parameter of the function represents the index of the source element.</param>
+        /// <param name="resultSelector">A function to create a result element from an element from 
+        /// the first sequence and a collection of matching elements from the second sequence.</param>
+        /// <returns>
+        /// A sequence whose elements are the result of invoking the one-to-many transform 
+        /// function <paramref name="collectionSelector"/> on each element of <paramref name="source"/> and then mapping 
+        /// each of those sequence elements and their corresponding source element to a 
+        /// result element.
+        /// </returns>
+        /// <exception cref="T:System.ArgumentNullException">
+        /// <paramref name="source"/> or <paramref name="collectionSelector"/> or
+        /// <paramref name="resultSelector"/> is a null reference (Nothing in Visual Basic).
+        /// </exception>
+        public static DistributedQuery<TResult> SelectMany<TSource, TCollection, TResult>(
+            this DistributedQuery<TSource> source, Func<TSource, int, IEnumerable<TCollection>> collectionSelector,
+            Func<TSource, TCollection, TResult> resultSelector)
+        {
+            if (source == null) throw new ArgumentNullException("source");
+            if (collectionSelector == null) throw new ArgumentNullException("collectionSelector");
+            if (resultSelector == null) throw new ArgumentNullException("resultSelector");
+
+            throw new NotImplementedException();
+            //return new SelectManyQueryOperator<TSource, TCollection, TResult>(source, null, collectionSelector, resultSelector);
+        }
     }
 }

68d9a2e40b3ef02a9b250849f795a5c1b5ba7e1b
Piotr Bazydło <piotr.bazydlo@student.put.poznan.pl>
2014-07-18 13:13:46 +0200

    Distributed PI test

---
+++ Bluepath.DistributedPI/Options.cs
@@ -0,0 +1,44 @@
+﻿using CommandLine;
+using CommandLine.Text;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluepath.DistributedPI
+{
+    class Options
+    {
+        [Option('i', "ip", Required = true)]
+        public string Ip { get; set; }
+
+        [Option('p', "port", Required = false)]
+        public int? Port { get; set; }
+
+        [Option('d', "discovery", Required = true)]
+        public string CentralizedDiscoveryURI { get; set; }
+
+        [Option('r', "redis", Required = true)]
+        public string RedisHost { get; set; }
+
+        [Option('s', "isSlave", Required = true)]
+        public int IsSlave { get; set; }
+
+        [Option('n', "no", Required = true)]
+        public int NoOfElements { get; set; }
+
+        [Option('a', "shards", Required = false)]
+        public int NoOfShards { get; set; }
+
+        [Option('b', "prefixes", Required = false)]
+        public int ReturnPrefixes { get; set; }
+
+        [HelpOption]
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ Bluepath.DistributedPI/Program.cs
@@ -0,0 +1,116 @@
+﻿using Bluepath.Framework;
+using Bluepath.Services;
+using Bluepath.Storage;
+using Bluepath.Storage.Structures.Collections;
+using Bluepath.Threading;
+using Bluepath.Threading.Schedulers;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bluepath.DistributedPI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                Bluepath.Log.RedisHost = options.RedisHost;
+                Bluepath.Log.WriteInfoToConsole = false;
+                var bluepathListener = new BluepathListener(options.Ip, options.Port);
+                using (var serviceDiscoveryClient
+                    = new CentralizedDiscovery.Client.CentralizedDiscovery(
+                        new ServiceUri(options.CentralizedDiscoveryURI, BindingType.BasicHttpBinding),
+                        bluepathListener
+                        )
+                      )
+                {
+                    using (var connectionManager = new ConnectionManager(
+                            remoteService: null,
+                            listener: bluepathListener,
+                            serviceDiscovery: serviceDiscoveryClient,
+                            serviceDiscoveryPeriod: TimeSpan.FromSeconds(30)))
+                    {
+                        System.Threading.Thread.Sleep(1500);
+                        var scheduler = new ThreadNumberScheduler(connectionManager);
+
+                        if (options.IsSlave == 0)
+                        {
+                            Log.TraceMessage(Log.Activity.Custom, "Running master");
+                            RunTest(connectionManager, scheduler, options);
+                        }
+                        else
+                        {
+                            Log.TraceMessage(Log.Activity.Custom, "Running slave");
+                        }
+
+                        Console.WriteLine("Press <Enter> to stop the service.");
+                        Console.ReadLine();
+                    }
+                }
+            }
+        }
+
+        private static void RunTest(ConnectionManager connectionManager, ThreadNumberScheduler scheduler, Options options)
+        {
+            var numberOfElements = options.NoOfElements;
+            var numberOfShards = options.NoOfShards;
+            var threads = new List<DistributedThread>();
+            Log.TraceMessage(Log.Activity.Custom, "Running test");
+            var sw = new Stopwatch();
+            sw.Start();
+            var processFunc = new Func<int, long>(
+                    (amount)
+                        =>
+                    {
+                        var r = new Random();
+                        long result = 0;
+                        for (int j = 0; j < amount; j++)
+                        {
+                            var x = r.NextDouble();
+                            var y = r.NextDouble();
+                            if (x * x + y * y < 1.0)
+                            {
+                                result++;
+                            }
+                        }
+
+                        return result;
+                    });
+            for (int i = 0; i < numberOfShards; i++)
+            {
+                var thread = DistributedThread.Create(
+                    processFunc,
+                    connectionManager,
+                    scheduler
+                    );
+                thread.Start(numberOfElements);
+                threads.Add(thread);
+            }
+
+            long sum = 0;
+            foreach (var thread in threads)
+            {
+                thread.Join();
+                if (thread.State == Executor.ExecutorState.Faulted)
+                {
+                    Console.WriteLine("Err");
+                }
+                else
+                {
+                    sum += (long)thread.Result;
+                }
+            }
+
+            var pi = 4.0 * sum / (numberOfElements * numberOfShards);
+            sw.Stop();
+            Console.WriteLine(string.Format("PI: {0}", pi));
+            Console.WriteLine(sw.ElapsedMilliseconds);
+        }
+    }
+}
